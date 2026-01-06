fcfd93fc27a825c7f4ec775fc9d69398d05d8658
raulb555 <raburgat@unitru.edu.pe>
2026-01-06 11:24:39 -0500

    Instalación del glm

---
+++ SimulacionMaquinaria/SimulacionMaquinaria/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <glm/glm.hpp> // Si no sale subrayado en rojo, ¡está bien configurado!
+
+int main() {
+    std::cout << "Proyecto de Computacion Grafica iniciado" << std::endl;
+    return 0;
+}
